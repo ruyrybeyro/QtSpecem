8f12f4db22dfd4a11ac652bcc8555bb5ede6d2ba
Rui Ribeiro <ruyrybeyro@gmail.com>
2019-11-02 01:18:15 +0000

    another screen test

--- Z80tests/screenfill4.asm
+++ Z80tests/screenfill4.asm
@@ -1,12 +1,40 @@
-;	FILL ZX SPECTRUM SCREEN GOING THROUGH EACH VISUAL LINE
+;	FILL ZX SPECTRUM SCREEN NORMAL
+;	AND THEN GOING THROUGH EACH VISUAL LINE
 ;	SLOWER AND PRINTING HL IN HEXADECIMAL
 ;
 ;	SCREEN ADDRESS  
 ;
 ;       H =  0  1  0 Y7 Y6 Y2 Y1 Y0
+;
+;	Y7-Y6 - section
+;	Y2-Y0 - lines 8x8
+;
 ;	L = Y5 Y4 Y3 X4 X3 X2 X1 X0
+;
+;	Y5-Y3 - line pixels
+;	X4-X0 - X
 
 	ORG	50000
+
+        LD      A,2             ; upper screen
+        CALL    5633            ; open channel
+
+        LD	A,(23695)
+	XOR	A
+	OR	$32
+	LD	(23695),A
+
+        LD      HL,$4000        ; SCREEN START
+
+LOOPC:  CALL	PRINT_STAT
+        LD      (HL),A
+        INC     HL
+
+        CALL    W_DELAY
+
+        LD      A,H
+        CP      $58
+        JR      NZ, LOOPC
 
 	LD	HL,$4000	; SCREEN START
 LOOP:
@@ -14,7 +42,7 @@ LOOP:
 
 	LD	B,32		; 32 COLUMNS
 X4_X0:	
-	CALL	PRINT_HL
+	CALL	PRINT_STAT
 
 	LD	(HL),A
 	INC	L
@@ -69,33 +97,169 @@ WAIT:	DJNZ	WAIT
         POP     BC
 	RET
 
-PRINT_HL:
+PRINT_STAT:
+
 	; PRINT AT
 	PUSH	HL
 	PUSH	AF
 	PUSH	BC
 
-	LD	B,27	 ; COL
-	LD	C,0	 ; LINE
-	LD      A,$16    ; PRINT AT
-
-	PUSH	HL
-	CALL	$201E	 ; CALL PR-AT-TAB
-	POP	HL
-
 	; PRINT HL
+	LD	B,20	 ; COL
+	LD	C,1	 ; LINE
+	CALL	PRINT_AT
+
+	LD	IX,TXTADDR
+	CALL	PRINT_STRING
+
 	LD	A,H
 	CALL	PRINT_A
 	LD	A,L
 	CALL	PRINT_A
 
-        LD      A,13            ; NEW LINE
-        RST     $10
+       ; PRINT HL
+        LD      B,20     ; COL
+        LD      C,2      ; LINE
+        CALL    PRINT_AT
+
+        LD      IX,TXTADDR2
+        CALL    PRINT_STRING
+
+        CALL    PRINTDEC
+
+
+	; PRINT SECTION
+        LD      B,20     ; COL
+        LD      C,3      ; LINE
+        CALL    PRINT_AT
+
+	LD      IX,TXTSEC
+        CALL    PRINT_STRING
+
+	LD	A,H
+	AND	$18
+	RRA
+	RRA
+	RRA
+	PUSH	AF
+	CALL    PRINTHEXA
+	POP	AF
+
+	RLA
+	RLA
+	RLA
+	LD	D,A
+
+        ; PRINT LINE 
+        LD      B,20     ; COL
+        LD      C,4      ; LINE
+        CALL    PRINT_AT
+
+        LD      IX,TXTLINE
+        CALL    PRINT_STRING
+
+	LD	A,L
+	AND	$E0
+	RRA
+	RRA
+	RRA
+	RRA
+	RRA
+	PUSH	AF
+	CALL	PRINTHEXA
+	POP	AF
+
+	ADD	A,D
+	RLA
+	RLA
+	RLA
+	LD	D,A	
+	
+
+       ; PRINT LINE CHR
+        LD      B,20     ; COL
+        LD      C,5      ; LINE
+        CALL    PRINT_AT
+
+        LD      IX,TXTCHR
+        CALL    PRINT_STRING
+
+        LD      A,H
+        AND     $07
+	PUSH	AF
+        CALL    PRINTHEXA
+	POP	AF
+
+	ADD	A,D
+	LD	D,A
+
+        ; PRINT COL
+        LD      B,20     ; COL
+        LD      C,6      ; LINE
+        CALL    PRINT_AT
+
+        LD      IX,TXTCOL
+        CALL    PRINT_STRING
+
+	PUSH	HL
+        LD      A,L
+        AND     $1F
+	LD	L,A
+	LD	H,0
+        CALL    PRINTDEC
+	POP	HL
+
+	; PRINT X
+        LD      B,20     ; COL
+        LD      C,7      ; LINE
+        CALL    PRINT_AT
+
+        LD      IX,TXT_X
+        CALL    PRINT_STRING
+
+        PUSH    HL
+        LD      A,L
+        AND     $1F
+        RLA
+        RLA
+        RLA
+        LD      L,A
+        LD      H,0
+        CALL    PRINTDEC
+        POP     HL
+
+        ; PRINT Y
+        LD      B,20     ; COL
+        LD      C,8      ; LINE
+        CALL    PRINT_AT
+
+        LD      IX,TXT_Y
+        CALL    PRINT_STRING
+
+	LD	H,0
+	LD	L,D
+	CALL	PRINTDEC
 
 	POP	BC
 	POP	AF
 	POP	HL
 	RET
+
+PRINT_AT:
+	LD      A,$16    ; PRINT AT
+
+        PUSH    HL
+        CALL    $201E    ; CALL PR-AT-TAB
+        POP     HL
+	RET
+
+PRINT_STRING:
+	LD	A,(IX)
+	CP	0
+	RET	Z
+	RST	$10
+	INC	IX
+	JR	PRINT_STRING
 
 ; =============================
 ; print "A" in hexa
@@ -133,6 +297,41 @@ ISDIGIT:
 
         POP     DE
         RET
+
+PRINTDEC:
+	PUSH	DE
+	PUSH	HL
+	LD	E,$20
+
+	LD	BC,$D8F0 ; -10000
+	CALL	$192A ; routine OUT-SP-NO outputs space or number
+
+	JP	$1A30	; OUT-NUM-3
+
+TXTADDR:
+	DEFB	"ADDR $"
+	DEFB	0
+TXTADDR2:
+        DEFB    "ADDR "
+        DEFB    0
+TXTSEC:
+	DEFB	"SECTION  "
+	DEFB	0
+TXTCOL:
+	DEFB	"COL  "
+	DEFB	0
+TXTLINE:
+	DEFB	"LINE     "
+	DEFB	0
+TXTCHR:
+	DEFB	"CHRLINE  "
+	DEFB	0
+TXT_X:
+	DEFB	"X    "
+	DEFB	0
+TXT_Y:
+	DEFB	"Y    "
+	DEFB	0
 
 	END 50000
 
