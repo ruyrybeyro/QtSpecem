6e1873c665788023e95665c847250073e9a76501
Rui Ribeiro <ruyrybeyro@gmail.com>
2025-02-22 17:13:06 +0000

    shm_server.c improvements

--- emul/shm_server.c
+++ emul/shm_server.c
@@ -1,4 +1,4 @@
-#if !defined(_WIN32) && !defined(WIN32)
+#if !defined(_WIN32)
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #endif
@@ -9,97 +9,86 @@
 #include "../h/env.h"
 
 #define SHMSZ     65536
-#define SHMVARS sizeof(struct Z80vars)+(sizeof(union Z80Regs)*2)+sizeof(struct CPU_flags) \
-		+sizeof(union Z80IX)+sizeof(union Z80IY)+1
+#define SHMVARS   (sizeof(struct Z80vars) + (sizeof(union Z80Regs) * 2) + sizeof(struct CPU_flags) + \
+                  sizeof(union Z80IX) + sizeof(union Z80IY) + 1)
 
-#if defined(_WIN32) || defined(WIN32)
+#define SHM_RAM_KEY  5678
+#define SHM_VARS_KEY 1234
+
+#if defined(_WIN32) 
 
 unsigned char * alloc_speccy_shared_ram(void)
 {
-   return calloc(SHMSZ,1);
+    unsigned char *mem = (unsigned char *)calloc(SHMSZ, 1);
+    if (!mem) {
+        perror("calloc failed for RAM");
+    }
+    return mem;
 }
 
 unsigned char * alloc_speccy_shared_vars(void)
 {
-   return calloc(SHMVARS,1);
+    unsigned char *vars = (unsigned char *)calloc(SHMVARS, 1);
+    if (!vars) {
+        perror("calloc failed for Vars");
+    }
+    return vars;
 }
 
-void dealloc_shared(unsigned char * mem, unsigned char * vars)
+void dealloc_shared(unsigned char *mem, unsigned char *vars)
 {
+    // Nothing to do on Windows since we use heap allocation
+    (void)mem;
+    (void)vars;
 }
 
-#else
+#else  // Unix (Linux, macOS, BSD, etc.)
 
 unsigned char * alloc_speccy_shared_ram(void)
 {
-    int shmid;
-    key_t key;
-    char *shm;
-
-    /*
-     * We'll name our shared memory segment
-     * "5678".
-     */
-    key = 5678;
-
-    /*
-     * Create the segment.
-     */
-    if ((shmid = shmget(key, SHMSZ, IPC_CREAT | 0666)) < 0) {
-        perror("shmget");
-        exit(1);
+    int shmid = shmget(SHM_RAM_KEY, SHMSZ, IPC_CREAT | 0666);
+    if (shmid < 0) {
+        perror("shmget failed for RAM");
+        return NULL;
     }
 
-    /*
-     * Now we attach the segment to our data space.
-     */
-    if ((shm = shmat(shmid, NULL, 0)) == (char *) -1) {
-        perror("shmat");
-        exit(1);
+    void *shm = shmat(shmid, NULL, 0);
+    if (shm == (void *)-1) {
+        perror("shmat failed for RAM");
+        return NULL;
     }
+
     return (unsigned char *)shm;
 }
 
 unsigned char * alloc_speccy_shared_vars(void)
 {
-    int shmid;
-    key_t key;
-    char *shm;
-
-    /*
-     * We'll name our shared memory segment
-     * "5678".
-     */
-    key = 1234;
-
-    /*
-     * Create the segment.
-     */
-    if ((shmid = shmget(key, SHMVARS, IPC_CREAT | 0666)) < 0) {
-        perror("shmget");
-        exit(1);
+    int shmid = shmget(SHM_VARS_KEY, SHMVARS, IPC_CREAT | 0666);
+    if (shmid < 0) {
+        perror("shmget failed for Vars");
+        return NULL;
     }
 
-    /*
-     * Now we attach the segment to our data space.
-     */
-    if ((shm = shmat(shmid, NULL, 0)) == (char *) -1) {
-        perror("shmat");
-        exit(1);
+    void *shm = shmat(shmid, NULL, 0);
+    if (shm == (void *)-1) {
+        perror("shmat failed for Vars");
+        return NULL;
     }
 
-    // counts number of "shm clients"
-    (*(shm+SHMVARS-1))++;
+    // Increase shared memory reference count
+    (*(unsigned char *)(shm + SHMVARS - 1))++;
     return (unsigned char *)shm;
 }
 
-void dealloc_shared(unsigned char * mem, unsigned char * vars)
+void dealloc_shared(unsigned char *mem, unsigned char *vars)
 {
-   if ( (--(*(vars+SHMVARS-1))) == 0 )
-   {
-      shmdt(vars);
-      shmdt(mem);
-   }
+    if (!vars) return; // Avoid null pointer access
+
+    if ((--(*(vars + SHMVARS - 1))) == 0) {
+        shmdt(vars);
+        shmdt(mem);
+    }
 }
 
 #endif
+
