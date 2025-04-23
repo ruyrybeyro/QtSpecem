1af14a84555b5829e499dcc027f8806cdb755b12
Rui Ribeiro <ruyrybeyro@gmail.com>
2025-04-23 18:34:16 +0100

    Debian  script improved

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -2,6 +2,18 @@
 # Tested with Debian 13
 sudo apt install git build-essential libsdl2-dev
 sudo apt install qt6-base-dev qt6-base-dev-tools qt6-declarative-dev qt6-multimedia-dev
+sudo apt install alsa-utils libasound2 libasound2-plugins pulseaudio
 #git clone https://github.com/ruyrybeyro/QtSpecem
 echo "Now you can run qmake"
+echo "If qmake does not run, run scripts/do_qmake.sh"
+echo to run sound, do "pulseaudio --start" first as the user running ./QtSpecem
+cat << EOF
+pcm.!default {
+    type pulse
+}
+
+ctl.!default {
+    type pulse
+}
+EOF
 
--- scripts/rh.sh
+++ scripts/rh.sh
@@ -5,3 +5,4 @@ sudo yum install SDL2 SDL2-devel
 sudo yum install mesa-libGL-devel
 sudo dnf install qt6-qtbase qt6-qtbase-devel qt6-qtdeclarative qt6-qtdeclarative-devel qt6-qtmultimedia qt6-qtmultimedia-devel
 echo "Now you can run qmake"
+echo "If qmake does not run, run scripts/do_qmake.sh"
